1b6bd28f84ca8e69f09115a400ab2a73c6c62a6b
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-11-07 20:49:48 -0300

    script sh

--- sh/compile.sh
+++ sh/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 JAVA_TOOL_OPTIONS="-Dhttps.protocols=TLSv1.2 -Djavax.net.ssl.trustStore=cacerts -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=JKS"
-./mvnw clean install -DskipTests=true
+./mvnw clean install -DskipTests=true 
